43665f9d9cb1d5573c2fd5fdae56888cea74f3e8
viktoriazaharova <zaharova181114@gmail.com>
2020-10-17 10:51:28 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,23 @@
+$('.slider-advantages').slick({
+    slidesToShow: 1,
+    dots: true,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+});
+
+$('.slider-products-top').slick({
+    slidesToShow: 1,
+    dots: true,
+    appendDots: '.slider-products-top__nav',
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+});
+
+$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+    $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+});
+
