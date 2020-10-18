95db0d206e7649552e28cacafea4d84d7c9c02dd
viktoriazaharova <zaharova181114@gmail.com>
2020-10-18 09:33:51 +0300

    edit

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,4 @@
+// slider
 $('.slider-advantages').slick({
     slidesToShow: 1,
     dots: true,
@@ -15,9 +16,25 @@ $('.slider-products-top').slick({
     nextArrow: '<button type="button" class="slick-next"></button>',
 });
 
+// tabs
 $('ul.tabs__caption').on('click', 'li:not(.active)', function () {
     $(this)
         .addClass('active').siblings().removeClass('active')
         .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
 });
 
+// input search
+$('[name="search"]').click(function(){
+    $('.search-dropDown').fadeIn();
+});
+
+
+$(document).mouseup(function (e) { // событие клика по веб-документу
+    var div = $('[name="search"]'); // тут указываем ID элемента
+    var btn = $('.search-dropDown');
+    if (!div.is(e.target) // если клик был не по нашему блоку
+        && !btn.is(e.target) && btn.has(e.target).length === 0
+        && div.has(e.target).length === 0) { // и не по его дочерним элементам
+        btn.fadeOut();
+    }
+});
